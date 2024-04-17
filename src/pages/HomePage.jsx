ffb73d7f1cf9a87dfc0ec7898dcdd2c10c3a2683
hackdanismo <office@justtoconfirm.co.uk>
2024-04-17 17:20:23 +0100

    feat: import and render the Button component into the Homepage template

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
+import Button from "../components/button"
+
 const HomePage = () => {
   return (
-    <h1>Homepage</h1>
+    <>
+      <h1>Homepage</h1>
+
+      <Button>Test Button</Button>
+    </>
   )
 }
 
