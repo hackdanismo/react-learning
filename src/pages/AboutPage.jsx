234b2150d78de77ae9da7980cbd0c292a0f77c47
hackdanismo <office@justtoconfirm.co.uk>
2024-04-25 14:13:06 +0100

    feat: import the Button component from the @hackdanismo/react-component-library component library and render on the About page template

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet"
+import { Button } from "@hackdanismo/react-component-library"
 
 const AboutPage = () => {
   return (
@@ -8,6 +9,9 @@ const AboutPage = () => {
       </Helmet>
 
       <h1>About</h1>
+
+      {/* This is a Button component from @hackdanismo/react-component-library */}
+      <Button />
     </>
   )
 }
